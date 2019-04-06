5af569ceac793fa5a2ae8a27c385a276766684ff
Moshe Praver <moshepraver@gmail.com>
2019-04-05 21:17:05 -0500

    getting data from arom api and rendering it

---
+++ src/Components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Results from "./Results";
+import Details from "./Details";
+import { Router, Link } from "@reach/router";
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<header>
+					<Link to="/">Adopt Me!</Link>
+				</header>
+				<Router>
+					<Results path="/" />
+					<Details path="/details/:id" />
+				</Router>
+			</div>
+		);
+	}
+}
